f6d75124c5902057ffcb53ab33015a48116d3c9f
Максим Котов <KM4252010@yandex.ru>
2023-04-14 18:04:30 +0300

    Create serializer

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'name',
+            'slug',
+        )
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = (
+            'name',
+            'slug',
+        )
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(
+        read_only=True,
+    )
+    genre = GenreSerializer(
+        read_only=True,
+        many=True,
+    )
+    rating = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Title
+        fields = (
+            'id',
+            'name',
+            'year',
+            'description',
+            'genre',
+            'category',
+            'rating',
+        )
+
+
+class TitleCreateSerializer(serializers.ModelSerializer):
+    ...
